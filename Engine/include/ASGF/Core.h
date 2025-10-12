d814f5fa033f1eb8c6d768eb7d025061102e54c0
Hosomi-PC <surendaotoko@gmail.com>
2025-10-12 16:34:30 +0100

    Add a new initialisation to reduce new project boilerplate

---
+++ Engine/include/ASGF/Core.h
@@ -0,0 +1,21 @@
+#include <type_traits>
+#include "Window.h"
+#include "Input.h"
+
+namespace ASGF
+{
+	class Simulation
+	{
+		Simulation();
+		virtual void Update() = 0;
+		virtual void Render() = 0;
+	};
+
+	template <typename _Ty>
+	concept _DerivedSimType = std::is_base_of<Simulation, _Ty>::value;
+	template<_DerivedSimType T>
+	void Run(int nWidth, int nHeight)
+	{
+
+	}
+}
