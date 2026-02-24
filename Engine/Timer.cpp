9188281487ee91df4d931c910605336e8e1f0dfe
Hosomi-PC <surendaotoko@gmail.com>
2026-02-24 01:58:52 +0000

    Extend timer functionality

--- Engine/Timer.cpp
+++ Engine/Timer.cpp
@@ -31,6 +31,12 @@ void Timer::Start()
     m_EndTime = std::chrono::system_clock::now() + m_Duration;
 }
 
+void Timer::Start(int nMilliseconds)
+{
+    SetDuration(nMilliseconds);
+    Start();
+}
+
 void Timer::SetDuration(int nMilliseconds)
 {
     m_Duration = std::chrono::milliseconds(nMilliseconds);
@@ -41,7 +47,22 @@ void Timer::SetDurationSecs(float fSeconds)
     m_Duration = std::chrono::milliseconds(static_cast<int>(fSeconds * 1000));
 }
 
+void Timer::ExtendDuration(int nMilliseconds)
+{
+    SetDuration(PeekTime() + nMilliseconds);
+    Start();
+}
+
 int Timer::PeekTime()
 {
     return std::chrono::duration_cast<std::chrono::milliseconds>(m_EndTime - std::chrono::system_clock::now()).count();
+}
+
+bool Timer::StartOrExtend(int nMilliseconds)
+{
+    if (Elapsed() || nMilliseconds > PeekTime())
+    {
+        SetDuration(nMilliseconds);
+        Start();
+    }
 }
--- Engine/include/ASGF/Timer.h
+++ Engine/include/ASGF/Timer.h
@@ -9,10 +9,14 @@ public:
 	bool Elapsed();
 	bool Peek();
 	void Start();
+	void Start(int nMilliseconds);
 	void SetDuration(int nMilliseconds);
 	void SetDurationSecs(float fSeconds);
+	void ExtendDuration(int nMilliseconds);
 	// returns the number of milliseconds remaining
 	int PeekTime();
+	// If the value is greater, extend the duration and restart, otherwise just start
+	bool StartOrExtend(int nMilliseconds);
 private:
 	std::chrono::system_clock::time_point m_EndTime;
 	std::chrono::system_clock::duration m_Duration;
